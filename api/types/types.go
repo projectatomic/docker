f9f86982915872890174815fda3aefd362acd667
Antonio Murdaca <runcom@redhat.com>
2015-11-19 12:27:29 +0100

    info: show registries
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- api/types/types.go
+++ api/types/types.go
@@ -194,6 +194,12 @@ type Version struct {
 	PkgVersion    string `json:",omitempty"`
 }
 
+// Registry holds information about a specific registry
+type Registry struct {
+	Name   string
+	Secure bool
+}
+
 // Info contains response of Remote API:
 // GET "/info"
 type Info struct {
@@ -241,6 +247,7 @@ type Info struct {
 	ServerVersion      string
 	ClusterStore       string
 	ClusterAdvertise   string
+	Registries         []Registry
 }
 
 // PluginsInfo is temp struct holds Plugins name
--- daemon/info.go
+++ daemon/info.go
@@ -1,6 +1,7 @@
 package daemon
 
 import (
+	"net"
 	"os"
 	"runtime"
 	"strings"
@@ -18,7 +19,7 @@ import (
 	"github.com/docker/docker/pkg/system"
 	"github.com/docker/docker/registry"
 	"github.com/docker/docker/utils"
-	"github.com/docker/docker/volume/drivers"
+	volumedrivers "github.com/docker/docker/volume/drivers"
 )
 
 // SystemInfo returns information about the host server the daemon is running on.
@@ -55,6 +56,14 @@ func (daemon *Daemon) SystemInfo() (*types.Info, error) {
 	initPath := utils.DockerInitPath("")
 	sysInfo := sysinfo.New(true)
 	packageVersion, _ := rpm.Version("/usr/bin/docker")
+
+	registries := []types.Registry{}
+	for _, ir := range daemon.RegistryService.Config.InsecureRegistryCIDRs {
+		registries = append(registries, types.Registry{(*net.IPNet)(ir).String(), false})
+	}
+	for n, i := range daemon.RegistryService.Config.IndexConfigs {
+		registries = append(registries, types.Registry{n, i.Secure})
+	}
 
 	v := &types.Info{
 		ID:                 daemon.ID,
@@ -93,6 +102,7 @@ func (daemon *Daemon) SystemInfo() (*types.Info, error) {
 		HTTPSProxy:         getProxyEnv("https_proxy"),
 		NoProxy:            getProxyEnv("no_proxy"),
 		PkgVersion:         packageVersion,
+		Registries:         registries,
 	}
 
 	// TODO Windows. Refactor this more once sysinfo is refactored into
