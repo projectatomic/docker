a0f9ea5679031576a826326581505f8a49a0c3cf
Dan Walsh <dwalsh@redhat.com>
2016-01-13 15:49:24 -0500

    Confirm a push to public Docker registry
    Ask the user to confirm a push of image to public Docker registry. The
    confirmation can be suppressed with the use of `-f, --force` flag.
    
    Added --confirm-def-push boolean flag to Docker daemon. It causes
    client to ask for confirmation before a push to public registry. It
    defaults to true. If set to false, push won't be restricted in any
    way unless the registry in question is blocked.
    
    No confirmation is needed for push to unofficial registry.
    
    Signed-off-by: Michal Minar <miminar@redhat.com>
    
    Signed-off-by: Dan Walsh <dwalsh@redhat.com>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -164,6 +164,7 @@ type Daemon struct {
 	imageStore                image.Store
 	nameIndex                 *registrar.Registrar
 	linkIndex                 *linkIndex
+	confirmDefPush            bool
 }
 
 // GetContainer looks for a container using the provided information, which could be
@@ -817,6 +818,7 @@ func NewDaemon(config *Config, registryService *registry.Service) (daemon *Daemo
 
 	d.nameIndex = registrar.NewRegistrar()
 	d.linkIndex = newLinkIndex()
+	d.confirmDefPush = config.ConfirmDefPush
 
 	if err := d.cleanupMounts(); err != nil {
 		return nil, err
@@ -1062,7 +1064,7 @@ func (daemon *Daemon) ExportImage(names []string, outStream io.Writer) error {
 }
 
 // PushImage initiates a push operation on the repository named localName.
-func (daemon *Daemon) PushImage(ref reference.Named, metaHeaders map[string][]string, authConfig *types.AuthConfig, outStream io.Writer) error {
+func (daemon *Daemon) PushImage(ref reference.Named, metaHeaders map[string][]string, authConfig *types.AuthConfig, force bool, outStream io.Writer) error {
 	// Include a buffer so that slow client connections don't affect
 	// transfer performance.
 	progressChan := make(chan progress.Progress, 100)
@@ -1088,6 +1090,8 @@ func (daemon *Daemon) PushImage(ref reference.Named, metaHeaders map[string][]st
 		ReferenceStore:   daemon.referenceStore,
 		TrustKey:         daemon.trustKey,
 		UploadManager:    daemon.uploadManager,
+		ConfirmDefPush:   daemon.confirmDefPush,
+		Force:            force,
 	}
 
 	err := distribution.Push(ctx, ref, imagePushConfig)
